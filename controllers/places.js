dae5ca36bc769b495e0360a6a42a368d9fa7c44a
annielam0623 <annzhouyy@gmail.com>
2023-02-13 13:32:43 -0800

    Sdding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -8,13 +8,13 @@ router.get('/', (req, res) => {
             city: 'Seattle',
             state: 'WA',
             cuisines: 'Thai, Pan-Asian',
-            pic: 'http://placekitten.com/250/250'
+            pic: '/images/tea-01.jpeg'
           }, {
             name: 'Coding Cat Cafe',
             city: 'Phoenix',
             state: 'AZ',
             cuisines: 'Coffee, Bakery',
-            pic: 'http://placekitten.com/250/250'
+            pic: '/images/coffee-01.jpeg'
           }]
            
     res.render('places/index', {places});    
--- views/default.jsx
+++ views/default.jsx
@@ -5,8 +5,11 @@ function Defualt (html) {
         <html>
             <head>
                 <title>Title</title>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"></link>
                 <link rel="stylesheet" href="/css/style.css" />
+                {/* <link  rel="stylesheet"
+					href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+					integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+					crossorigin="anonymous"/> */}
             </head>
             <body>
                 {html.children}
