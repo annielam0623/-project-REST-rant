e3de8d878c4c19522af4ac37ef0ce967f0443d9c
annielam0623 <annzhouyy@gmail.com>
2023-02-13 21:20:07 -0800

    Jazzedup form page

--- views/default.jsx
+++ views/default.jsx
@@ -6,10 +6,10 @@ function Defualt (html) {
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href="/css/style.css" />
-                {/* <link  rel="stylesheet"
+                <link  rel="stylesheet"
 					href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
 					integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-					crossorigin="anonymous"/> */}
+					crossorigin="anonymous"/>
             </head>
             <body>
                 {html.children}
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -6,11 +6,11 @@ function index(data) {
     return (
       <div className="col-sm-6">
         <h2>{place.name}</h2>
-        <p className="text-center">
+        <p className="text-left">
           {place.cuisines}
         </p>
         <img src={place.pic} alt={place.name} />
-        <p className="text-center">
+        <p className="text-leftr">
           Located in {place.city}, {place.state}
         </p>
       </div>
