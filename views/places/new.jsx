fb9856f34d5fa3ff379ea01fcdc42fbc421986b9
annielam0623 <annzhouyy@gmail.com>
2023-02-13 20:14:56 -0800

    Addingand rendering a new view

---
+++ views/places/new.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('../default')
+
+function new_form() {
+    return (
+        <Def>
+            <mian>
+                <h1>Add a New Place</h1>
+            </mian>
+        </Def>
+    )
+
+}
+
+module.exports = new_form
