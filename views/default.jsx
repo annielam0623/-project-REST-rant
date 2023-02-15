9c064217299106a4c91875af234180a0a1e7a095
annielam0623 <annzhouyy@gmail.com>
2023-02-14 21:11:37 -0800

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -12,6 +12,20 @@ function Defualt (html) {
 					crossorigin="anonymous"/>
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
         </html>
