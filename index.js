622600e1c1d4e7dd1574085166efd67801ff54d8
annielam0623 <annzhouyy@gmail.com>
2023-02-19 19:23:11 -0800

    updated Index and Show

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const mongoose =require('mongoose')
+const MONGO_URI = process.env.MONGO_URI
 const methodOverride = require('method-override');
 
 
@@ -12,7 +15,15 @@ app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+mongoose.set({strictQuery: true})
 
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true},
+	() => {
+		console.log('connected to:', MONGO_URI);
+	}
+)
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'));
@@ -27,3 +38,5 @@ app.get('*', (req, res) => {
 
 //Listen for Connections
 app.listen(process.env.PORT)
+
+module.exports.Place = require('./models/places')
--- models/places.js
+++ models/places.js
@@ -1,17 +1,37 @@
-module.exports = [
+//Require mongoose
+const mongoose = require('mongoose')
 
-{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/tea-01.jpeg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-01.jpeg'
-      }]
+//Create shorthand for the Schema constructor
+const {Schema} = mongoose
+
+const placeSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  pic: String,
+  cuisines: {type: String, required: true},
+  city: {type:String, default: 'Anytown'},
+  state: {type: String, default: 'USA'},
+  founded: Number
+})
+
+
+module.exports = mongoose.model('Place', placeSchema)
+
+
+
+// module.exports = [
+
+// {
+//         name: 'H-Thai-ML',
+//         city: 'Seattle',
+//         state: 'WA',
+//         cuisines: 'Thai, Pan-Asian',
+//         pic: '/images/tea-01.jpeg'
+//       }, {
+//         name: 'Coding Cat Cafe',
+//         city: 'Phoenix',
+//         state: 'AZ',
+//         cuisines: 'Coffee, Bakery',
+//         pic: '/images/coffee-01.jpeg'
+//       }]
        
 
